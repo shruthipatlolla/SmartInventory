43329a6610f827d4c207baab701efc6b7d90d836
Patlolla,Shruthi <s531376@ch140004m.nwmissouri.edu>
2018-10-05 17:34:50 -0500

    Designed Registration page

---
+++ Smart Inventory/Smart Inventory/RegistrationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegistrationViewController.swift
+//  Smart Inventory
+//
+//  Created by Patlolla,Shruthi on 10/5/18.
+//  Copyright © 2018 Jennaikode,Vamshi Raj. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func onRegister(_ sender: Any) {
+    }
+    
+}
