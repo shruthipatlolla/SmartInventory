ea213aa40d259fef6f20dbf61ba96677390d99e7
Vamshi Raj <student@nw46143.local>
2018-12-02 17:37:40 -0600

    Added new type of password reset. Added proper prompts forgot password

--- Smart Inventory/Smart Inventory/ForgotPasswordViewController.swift
+++ Smart Inventory/Smart Inventory/ForgotPasswordViewController.swift
@@ -10,28 +10,38 @@ import UIKit
 
 
 class ForgotPasswordViewController: UIViewController {
-
-    let backendless = Backendless.sharedInstance()
+    
+    //let backendless = Backendless.sharedInstance()
     @IBOutlet weak var emailTF: UITextField!
+    
+    
+    func display(title:String, msg:String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func onSubmit(_ sender: Any) {
-        
-        let email = emailTF.text
-        backendless?.userService.restorePassword(email)
-        
+        Backendless.sharedInstance()!.userService.restorePassword(emailTF.text!, response: {
+            self.display(title: "Check Inbox", msg: "If you have entered correct email you will get an mail to reset the password")
+        }, error: { fault in
+            self.display(title: "Something is Wrong", msg: (fault?.message!)!)
+        })
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
