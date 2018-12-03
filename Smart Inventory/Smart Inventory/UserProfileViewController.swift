b7bc2d91a722a6f9500d7c659d73b208deb31b97
Manogna <student@nw46143.local>
2018-12-02 18:52:50 -0600

    Displayed logged in user details in profile View Controller

--- Smart Inventory/Smart Inventory/UserProfileViewController.swift
+++ Smart Inventory/Smart Inventory/UserProfileViewController.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 
+    @IBOutlet weak var nameLBL: UILabel!
+    @IBOutlet weak var emailLBL: UILabel!
+    @IBOutlet weak var numberLBL: UILabel!
+    @IBOutlet weak var addressLBL: UILabel!
+    @IBOutlet weak var addres2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        let user  = Backendless.sharedInstance()
+        
+        
+        nameLBL.text = user?.userService.currentUser.getProperty("name") as? String
+        emailLBL.text = user?.userService.currentUser.getProperty("email") as? String
+        numberLBL.text = user?.userService.currentUser.getProperty("mobile") as? String
+        addressLBL.text = user?.userService.currentUser.getProperty("address") as? String
+        addres2.text = ("\(user?.userService.currentUser.getProperty("city") as! String), \(user?.userService.currentUser.getProperty("state") as! String) \(user?.userService.currentUser.getProperty("zip") as! String)")
+
+        
+    }
 
     /*
     // MARK: - Navigation
